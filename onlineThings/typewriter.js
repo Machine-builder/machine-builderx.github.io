893534b7622a9cbc82fe0b1c12499d66a3c5c299
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2019-06-16 19:30:14 +1000

    Update typewriter.js

--- onlineThings/typewriter.js
+++ onlineThings/typewriter.js
@@ -2,10 +2,10 @@
 var aText = new Array( "ph" );
 
 function iSpeed() {
-    return Math.floor(Math.random()*140) // anywhere between 0-x
+    return Math.floor(Math.random()*80) // anywhere between 0-x
 }
 function lSpeed() {
-    return Math.floor(Math.random()*800) // anywhere between 0-x
+    return Math.floor(Math.random()*400) // anywhere between 0-x
 }
 
 var iIndex = 0; // start printing array at this posision
