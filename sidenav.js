8f07afeb6ebd461f722e41201662d3ba52b4f60c
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2019-12-29 17:24:09 +1100

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
