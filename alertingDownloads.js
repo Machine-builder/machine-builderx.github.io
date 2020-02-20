137b3885b8ab097c7e75d7fb5df0ad769a89d0a6
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2020-02-20 19:49:48 +1100

    Add files via upload

--- alertingDownloads.js
+++ alertingDownloads.js
@@ -1,12 +1,27 @@
+function goToURL(url) {
+    window.location.href = url
+
+}
+
 function downloadFeatureGen() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        openURL('https://drive.google.com/file/d/15eBRSl-uZALnhHZdoqRSd2J8f3QIqomn/view?usp=sharing')
     } else {
-
+        goToURL('https://drive.google.com/file/d/15eBRSl-uZALnhHZdoqRSd2J8f3QIqomn/view?usp=sharing')
     }
 }
 
-function openURL(url) {
-    Object.assign(document.createElement('a'), { target: '_blank', href: url}).click();
+function downloadFeatureGen_beta_online() {
+    var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
+    if (r == true) {
+    } else {
+        goToURL('https://drive.google.com/file/d/1buheAxw7gswywdpugXzQf7UR5vLTeXM8/view?usp=sharing')
+    }
+}
+function downloadFeatureGen_beta_offline() {
+    var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
+    if (r == true) {
+    } else {
+        goToURL('https://drive.google.com/file/d/15dFJkBcVv-P805g1eDdgHyKpocOjkYns/view?usp=sharing')
+    }
 }
