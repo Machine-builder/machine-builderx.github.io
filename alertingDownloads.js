bd3f33acde8ba40c7a462fd134e5a1a7a61cd164
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2020-02-20 19:55:03 +1100

    Add files via upload

--- alertingDownloads.js
+++ alertingDownloads.js
@@ -6,22 +6,22 @@ function goToURL(url) {
 function downloadFeatureGen() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-    } else {
         goToURL('https://drive.google.com/file/d/15eBRSl-uZALnhHZdoqRSd2J8f3QIqomn/view?usp=sharing')
+    } else {
     }
 }
 
 function downloadFeatureGen_beta_online() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-    } else {
         goToURL('https://drive.google.com/file/d/1buheAxw7gswywdpugXzQf7UR5vLTeXM8/view?usp=sharing')
+    } else {
     }
 }
 function downloadFeatureGen_beta_offline() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-    } else {
         goToURL('https://drive.google.com/file/d/15dFJkBcVv-P805g1eDdgHyKpocOjkYns/view?usp=sharing')
+    } else {
     }
 }
