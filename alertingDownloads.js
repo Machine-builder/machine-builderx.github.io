abca561437bf09a115cf0bbc1c08ca0ba8b3f8ee
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2020-02-22 10:47:35 +1100

    Add files via upload

--- alertingDownloads.js
+++ alertingDownloads.js
@@ -6,7 +6,7 @@ function goToURL(url) {
 function downloadFeatureGen() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        goToURL('https://drive.google.com/file/d/15eBRSl-uZALnhHZdoqRSd2J8f3QIqomn/view?usp=sharing')
+        goToURL('https://drive.google.com/file/d/1Q1dr-WTbP0oKDsEeLcB67nXnDpFUtc7d/view?usp=sharing')
     } else {
     }
 }
@@ -14,14 +14,14 @@ function downloadFeatureGen() {
 function downloadFeatureGen_beta_online() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        goToURL('https://drive.google.com/file/d/1MKQUsYBkyTpEQpFE2FlizIpQuND_a7E_/view?usp=sharing')
+        goToURL('https://drive.google.com/file/d/1hntrX-agp7S8c5T38i2kyOSfBwWM7Llo/view?usp=sharing')
     } else {
     }
 }
 function downloadFeatureGen_beta_offline() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        goToURL('https://drive.google.com/file/d/1vRInVgQMsRF0yt65ZIWIu5uc32Z_tNY0/view?usp=sharing')
+        goToURL('https://drive.google.com/file/d/1BK9u9ycTgYa-LWenq-Q5njLjZ3-8qvul/view?usp=sharing')
     } else {
     }
 }
