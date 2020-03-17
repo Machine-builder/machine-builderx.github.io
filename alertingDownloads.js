7ee6055f7b84a158c51b4fe913875c023281218c
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2020-03-17 16:05:31 +1100

    Add files via upload

--- alertingDownloads.js
+++ alertingDownloads.js
@@ -14,14 +14,14 @@ function downloadFeatureGen() {
 function downloadFeatureGen_beta_online() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        goToURL('https://drive.google.com/file/d/1NCp_ggTISQiBr_rdJRkUubJkHWvIxb1i/view?usp=sharing')
+        goToURL('https://drive.google.com/file/d/1oHL1JowCKMN7toAHM3Vc6TCCVwDImGkO/view?usp=sharing')
     } else {
     }
 }
 function downloadFeatureGen_beta_offline() {
     var r = confirm("By pressing OK, you agree that if you create and upload an addon with the help of this software, you must provide credit as a link to the original video (https://www.youtube.com/watch?v=vcLibdIq7To). If you do not give credit in your addon, Your addon will be taken down.")
     if (r == true) {
-        goToURL('https://drive.google.com/file/d/14FnMWuayeqKGEJ0T_0MCIJEaPxwtCkTM/view?usp=sharing')
+        goToURL('https://drive.google.com/file/d/11jG7JvpP9x70ZEjjw4AnrsnyELqFVvxb/view?usp=sharing')
     } else {
     }
 }
