b244b5361066b7c54344da86918c44c92948a26e
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2021-03-17 17:42:53 +1100

    Delete links.js

--- links.js
+++
@@ -1,5 +0,0 @@
-// https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window
-function openInNewTab(url) {
-    var win = window.open(url, '_blank');
-    win.focus();
-}
